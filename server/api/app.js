3c957ab7173f74e268f917c4f95dac0c419e9a38
yakupkalayci <ykpklyc18@gmail.com>
2026-07-05 17:36:47 +0300

    update cors

--- server/api/app.js
+++ server/api/app.js
@@ -18,8 +18,12 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://mylanguagecenter.tech",
+  "https://mylanguagecenter.tech",
+];
 app.use(cors({
-    origin: "https://mylanguagecenter.tech",
+    origin: allowedOrigins,
     credentials: true,
 }));
 app.use(express.static(path.join(__dirname, 'public')));
